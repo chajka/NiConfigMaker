445ffa0daee126e08f8817f3bb176a8e66062ba4
Я Чайка <mellanie335@gmail.com>
2013-03-17 11:00:20 +0900

    Add constant definition header file into project.

---
+++ NiConfigMaker/NiConfigMakerDefinitions.h
@@ -0,0 +1,98 @@
+//
+//  NiConfigMakerDefinitions.h
+//  NiConfigMaker
+//
+//  Created by Чайка on 3/16/13.
+//  Copyright (c) 2013 Instrumentality of mankind. All rights reserved.
+//
+#pragma once
+
+#ifndef NiConfigMaker_NiConfigMakerDefinitions_h
+#define NiConfigMaker_NiConfigMakerDefinitions_h
+
+typedef NSInteger VideoSize;
+enum {
+	VideoSize88x72 = 1001,
+	VideoSize128x96,
+	VideoSize160x80,
+	VideoSize160x120,
+	VideoSize160x240,
+	VideoSize176x144,
+	VideoSize240x176,
+	VideoSize240x180,
+	VideoSize256x192,
+	VideoSize320x180,
+	VideoSize320x192,
+	VideoSize320x240,
+	VideoSize352x240,
+	VideoSize352x288,
+	VideoSize384x288,
+	VideoSize480x288,
+	VideoSize480x360,
+	VideoSize640x240,
+	VideoSize640x288,
+	VideoSize640x360,
+	VideoSize640x480,
+	VideoSize648x486,
+	VideoSize704x480,
+	VideoSize704x576,
+	VideoSize720x240,
+	VideoSize720x288,
+	VideoSize720x380,
+	VideoSize720x400,
+	VideoSize720x480,
+	VideoSize720x576,
+	VideoSize768x576,
+	VideoSize1024x768,
+	VideoSize1024x1080,
+	VideoSize1280x720,
+	VideoSize1280x1080,
+	VideoSize1920x1080
+};
+
+typedef NSInteger FrameRate;
+enum {
+	FrameRate60_00,
+	FrameRate59_94,
+	FrameRate30_00,
+	FrameRate29_97,
+	FrameRate25_00,
+	FrameRate24_00,
+	FrameRate20_00,
+	FrameRate15_00,
+	FrameRate14_98,
+	FrameRate10_00,
+	FrameRate08_00,
+	FrameRate06_00,
+	FrameRate05_00,
+	FrameRate04_00,
+	FrameRate01_00
+};
+
+typedef NSInteger SampleRate;
+enum {
+	SampleRate22050,
+	SampleRate11025,
+	SampleRate8500,
+	SampleRate5512
+};
+
+typedef NSInteger AudioBitRate;
+enum {
+	AudioBitRate224,
+	AudioBitRate192,
+	AudioBitRate160,
+	AudioBitRate128,
+	AudioBitRate112,
+	AudioBitRate96,
+	AudioBitRate64,
+	AudioBitRate56,
+	AudioBitRate48,
+	AudioBitRate40,
+	AudioBitRate32,
+	AudioBitRate24,
+	AudioBitRate20,
+	AudioBitRate18
+};
+
+#endif
